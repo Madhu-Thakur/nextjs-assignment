8650254ae90633d06f56a1278c1d73d33bad4fa9
Madhu <madhuthakur2321998@gmail.com>
2026-07-27 21:25:18 +0530

    Added static image from public folder

--- app/products/page.js
+++ app/products/page.js
@@ -1,32 +1,17 @@
-import Link from "next/link";
-
-async function getProducts() {
-  const res = await fetch("https://dummyjson.com/products");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch products");
-  }
-
-  const data = await res.json();
-  return data.products;
-}
-
-export default async function ProductsPage() {
-  const products = await getProducts();
+export default async function ProductPage({ params }) {
+  const { id } = await params;
 
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Product {id}</h1>
 
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>
-            <Link href={`/products/${product.id}`}>
-              {product.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <img
+        src="/product.png"
+        alt="Product"
+        width={250}
+      />
+
+      <p>Product {id} details page</p>
     </div>
   );
 }
